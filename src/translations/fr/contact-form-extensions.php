8eafe5deac05737356932e66ccdc1128c484cf81
Pascal Garneau <pascal@ascense.ca>
2023-08-17 11:57:08 -0400

    fix styleci

--- src/translations/fr/contact-form-extensions.php
+++ src/translations/fr/contact-form-extensions.php
@@ -14,17 +14,17 @@
  * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
  */
 return [
-    'Submissions'      => 'Soumissions',
-    'Form Submissions' => 'Soumissions de formulaires',
-    'All submissions'  => 'Toutes les soumissions',
-    'Form'             => 'Formulaire',
-    'Subject'          => 'Sujet',
-    'From Name'        => 'Nom',
-    'From name'        => 'Nom',
-    'From Email'       => 'Courriel',
-    'From email'       => 'Courriel',
-    'Message'          => 'Message',
-    'Date Created'     => 'Date de création',
-    'Back to overview' => 'Retour à l\'aperçu',
-    'Are you sure you want to delete the selected submissions?' => 'Voulez-vous vraiment supprimer les soumissions sélectionnées?'
+    'Submissions'                                               => 'Soumissions',
+    'Form Submissions'                                          => 'Soumissions de formulaires',
+    'All submissions'                                           => 'Toutes les soumissions',
+    'Form'                                                      => 'Formulaire',
+    'Subject'                                                   => 'Sujet',
+    'From Name'                                                 => 'Nom',
+    'From name'                                                 => 'Nom',
+    'From Email'                                                => 'Courriel',
+    'From email'                                                => 'Courriel',
+    'Message'                                                   => 'Message',
+    'Date Created'                                              => 'Date de création',
+    'Back to overview'                                          => 'Retour à l\'aperçu',
+    'Are you sure you want to delete the selected submissions?' => 'Voulez-vous vraiment supprimer les soumissions sélectionnées?',
 ];
