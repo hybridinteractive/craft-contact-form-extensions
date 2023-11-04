966a1406a0cf4860078e3e694738419f7fbaef7f
Ross Kenney <ross@rosskenney.com>
2023-11-04 18:23:42 -0400

    Adding other translations.

---
+++ src/translations/de/contact-form-extensions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Craft Contact Form Extensions plugin for Craft CMS 4.x.
+ *
+ * Adds extensions to the Craft CMS contact form plugin.
+ */
+/**
+ * Craft Contact Form Extensions en Translation.
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('craft-contact-form-extensions', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ */
+return [
+    'Form Submissions' => 'Formulareinreichungen',
+    'All submissions'  => 'Alle Einreichungen',
+    'Form'             => 'Formular',
+    'Subject'          => 'Betreff',
+    'From Name'        => 'Absender Name',
+    'From Email'       => 'Absender E-Mail',
+    'Message'          => 'Nachricht',
+    'Date Created'     => 'Erstellungsdatum',
+    'Back to overview' => 'Zurück zur Übersicht',
+    'Flat'             => 'Flach',
+];
